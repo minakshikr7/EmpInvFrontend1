83d4371b80c2d48b9f2290f1f7f299c64eaf9468
minakshikr7 <minakshi052005@gmail.com>
2025-06-22 23:24:33 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,33 +38,7 @@ const Header = () => {
   );
 };
 
-const NavContent = ()=>{
-    <>
-    <div>
-      <a  href="#home">
-        Home
-      </a>
-      <a  href="#work">
-        Work
-      </a>
-      <a  href="#timeline">
-        Experience
-      </a>
-      <a  href="#services">
-        Services
-      </a>
-      <a  href="#testimonial">
-        Testimonial
-      </a>
-      <a  href="#contact">
-        Contact
-      </a>
-    </div>
-    <a href="mailto:official.6packprogrammer@gmail.com">
-      <button>Email</button>
-    </a>
-    </>
-}
+
 
 
 
